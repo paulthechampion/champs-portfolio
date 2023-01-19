a1583d8bd83328450c232ee9ff1911d513c8adcc
paulthechampion <enwerempaulo@gmail.com>
2023-01-18 23:35:19 -0500

    Done finally

--- src/component/Main.js
+++ src/component/Main.js
@@ -25,6 +25,7 @@ import keer from "./img/kreer.jpg"
 import todoPic from "./img/todo.png"
 import ruixPic from "./img/rubixx.png"
 import nadaPic from "./img/nada-nada.jpg"
+import abdul from "./img/abdul.jpg"
 import surveyPic from "./img/survey.jpg"
 import usingLaptop from "./img/using-laptop.gif" 
 import sun from "./img/sun-solid.png"
@@ -245,7 +246,7 @@ export default function Main() {
                             {name: "NADA Create", img: nadaPic, link: "https://nada-create.herokuapp.com/", github: "https://github.com/paulthechampion/nada-create", words:"Nada Create is an ongiong Ecommerce company which sells sportwears, especially that of soccer. This Application is being created using Node js, Express, Ejs for the view engine, Mongodb, Paystack for the Payment integration system and many more"}, 
                             {name: "Clown Chat", img: myPic1, link: "https://clownchat.herokuapp.com/",github : "https://github.com/paulthechampion/ClownChat", words:"A comical messaging app, created using Express, Node js, Ejs view engine and Socket.io"},
                             {name: "Read Rubixx", img: ruixPic, link: "https://rubixx.herokuapp.com/",github : "https://github.com/paulthechampion/rubix", words:"Rubixx is a Full-stack online book library, it saves authors and their books. Also enables users to read books. This as created using the MERN stack as well as PDF.js library to read the books"},  
-                            {name: "AbdulAuwal.com", img: myPic1, link: "https://www.abdulauwal.com/",github : "https://github.com/paulthechampion/nada", words:"A personal portfolio website for the most amazing UI/Ux Developer out there right now, check it ouT!!!. It was created using React JS, Node Js and Express"},
+                            {name: "AbdulAuwal.com", img: abdul, link: "https://www.abdulauwal.com/",github : "https://github.com/paulthechampion/nada", words:"A personal portfolio website for the most amazing UI/Ux Developer out there right now, check it ouT!!!. It was created using React JS, Node Js and Express"},
                             {name: "Emaily", img: surveyPic, link: "https://email-dev-list.herokuapp.com",github : "https://github.com/paulthechampion/email-list", words:"Emaily is a survey dispatch service, it enables the users to send surveys to a series of emails and receive feedback. This app is still in the test phase, it was created using Mongodb, Express Js, React Js, Redux, Redux Forms, Stripe for payment integration, SendGrid,Node js and many more"}, 
                             {name: "Todo App", img: todoPic, link: "https://champs-todo.herokuapp.com",github : "https://github.com/paulthechampion/todoApp", words:"A Full-stack Todo app, saves Todo tasks and arranges them in a tabular format"}
                         ]
