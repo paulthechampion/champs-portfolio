4e39cbb2bba59c8629d9ea060aeb316a0f9aad89
paulthechampion <enwerempaulo@gmail.com>
2023-10-17 00:51:30 -0400

    squre one

--- src/components/SwipeDetector.js
+++ src/components/SwipeDetector.js
@@ -2,29 +2,32 @@ import React, { useState } from 'react';
 import { useSwipeable } from 'react-swipeable';
 
 function SwipeDetector() {
-    const [backgroundColor, setBackgroundColor] = useState('lightgray');
+  const [backgroundColor, setBackgroundColor] = useState('lightgray');
 
-    const changeBackgroundColor = (newColor) => {
-        setBackgroundColor(newColor);
-    };
+  const handlers = useSwipeable({
+    onSwiped: (eventData) => {
+      if (eventData.dir === 'Up') {
+        setBackgroundColor('red');
+      } else if (eventData.dir === 'Down') {
+        setBackgroundColor('green');
+      }
+    },
+  });
 
-    const handlers = useSwipeable({
-        onSwiped: (eventData) => {
-            if (eventData.dir === 'Up') {
-                // Handle swipe up
-                changeBackgroundColor('blue'); // Change to the desired background color
-            } else if (eventData.dir === 'Down') {
-                // Handle swipe down
-                changeBackgroundColor('green'); // Change to the desired background color
-            }
-        },
-    });
+  const swipeAreaStyle = {
+    width: '100%',
+    height: '100vh', // Full viewport height
+    backgroundColor,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  };
 
-    return (
-        <div {...handlers} id="swipe-div" style={{ width: '100%', height: '100vh', backgroundColor: backgroundColor, zIndex: 1, position: 'fixed', top: 0 }}>
-            {/* Your swipeable content */}
-        </div>
-    );
+  return (
+    <div {...handlers} style={swipeAreaStyle}>
+      Swipe me up or down
+    </div>
+  );
 }
 
 export default SwipeDetector;
