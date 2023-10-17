82e01ea85e15415cd9a50367f87d35d5dc567a2c
paulthechampion <enwerempaulo@gmail.com>
2023-10-16 20:36:11 -0400

    Phone Swipe uninstalled last install and using react-swipe-component now

--- src/components/SwipeDetector.js
+++ src/components/SwipeDetector.js
@@ -1,26 +1,46 @@
-import React from 'react';
-import { Hammer } from 'react-hammerjs';
+import React, {Component} from 'react';
+import Swipe from 'react-swipe-component';
 
-class SwipeDetector extends React.Component {
-  handleSwipe = (event) => {
-    if (event.direction === 8) {
-        //swiped up
-        document.getElementById("hammer").style.backgroundColor = "red"
-    } else if (event.direction === 16) {
-      // Swiped down
-      document.getElementById("hammer").style.backgroundColor = "green"
+class SwipeDetector extends Component{
+    constructor(){
+        super();
+        this.onSwipeLeftListener = this._onSwipeLeftListener.bind(this);
+        this.onSwipeRightListener = this._onSwipeRightListener.bind(this);
+        this.onSwipeDownListener = this._onSwipeUpListener.bind(this);
+        this.onSwipeUpListener = this._onSwipeDownListener.bind(this);
+        this.onSwipeListener = this._onSwipeListener.bind(this);
     }
-  };
-
-  render() {
-    return (
-      <Hammer onSwipe={this.handleSwipe}>
-        <div id="hammer" style={{ width: '100%', height: '300px', backgroundColor: 'lightgray' }}>
-          Swipe me Up or Down
-        </div>
-      </Hammer>
-    );
-  }
+    render() {
+        return (
+        <Swipe
+            nodeName="div"
+            id="swipe"
+            className="swipe"
+            mouseSwipe={false}
+            onSwipedLeft={this.onSwipeLeftListener}
+            onSwipedRight={this.onSwipeRightListener}
+            onSwipedDown={this.onSwipeDownListener}
+            onSwipedUp={this.onSwipeUpListener}
+            onSwipe={this.onSwipeListener}
+        >
+            Swipe
+        </Swipe>);
+    }
+    _onSwipeLeftListener(){
+        console.log("Swiped left");
+    }
+    _onSwipeRightListener(){
+        console.log("Swiped right");
+    }
+    _onSwipeUpListener(){
+        document.getElementById('swipe').style.backgroundColor('red')
+    }
+    _onSwipeDownListener() {
+        document.getElementById('swipe').style.backgroundColor('green')
+    }
+    _onSwipeListener(e){
+        if (e[1]===0) console.log("Swipe x: "+e[0]);
+        else if (e[0]===0) console.log("Swipe y: "+e[1]);
+    }
 }
-
 export default SwipeDetector;
