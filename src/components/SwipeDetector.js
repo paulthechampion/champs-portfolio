ecc4c0291a6b0098d80a44c4f7c8af0edee3a7de
paulthechampion <enwerempaulo@gmail.com>
2023-10-17 00:45:33 -0400

    sighjjjjjjjjjjjjjjjjjjjjj

--- src/components/SwipeDetector.js
+++ src/components/SwipeDetector.js
@@ -2,38 +2,26 @@ import React, { useState } from 'react';
 import { useSwipeable } from 'react-swipeable';
 
 function SwipeDetector() {
-    const [activeCard, setActiveCard] = useState(1);
-    const [backgroundColor, setBackgroundColor] = useState(1);
+    const [backgroundColor, setBackgroundColor] = useState('lightgray');
 
-    const scrollToElement = (elementId) => {
-        const element = document.getElementById(elementId);
-        if (element) {
-            element.scrollIntoView({ behavior: 'smooth', block: 'start' });
-        }
+    const changeBackgroundColor = (newColor) => {
+        setBackgroundColor(newColor);
     };
 
     const handlers = useSwipeable({
         onSwiped: (eventData) => {
             if (eventData.dir === 'Up') {
-                setBackgroundColor('red')
                 // Handle swipe up
-                if (activeCard > 1) {
-                    scrollToElement(`project${activeCard - 1}`);
-                    setActiveCard(activeCard - 1);
-                }
+                changeBackgroundColor('blue'); // Change to the desired background color
             } else if (eventData.dir === 'Down') {
-                setBackgroundColor('yellow')
                 // Handle swipe down
-                if (activeCard < 7) {
-                    scrollToElement(`project${activeCard}`);
-                    setActiveCard(activeCard + 1);
-                }
+                changeBackgroundColor('green'); // Change to the desired background color
             }
         },
     });
 
     return (
-        <div {...handlers} id="swipe-div" style={{ width: '100%', height: '100vh', backgroundColor, zIndex: 1, position: 'fixed', top: 0 }}>
+        <div {...handlers} id="swipe-div" style={{ width: '100%', height: '100vh', backgroundColor: backgroundColor, zIndex: 1, position: 'fixed', top: 0 }}>
             {/* Your swipeable content */}
         </div>
     );
