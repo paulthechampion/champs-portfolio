7ce85b9f804ef1cf0beef7d327646930d9fe533f
paulthechampion <enwerempaulo@gmail.com>
2023-10-17 03:30:36 -0400

    it was better before

--- src/components/SwipeDetector.js
+++ src/components/SwipeDetector.js
@@ -1,9 +1,11 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { useSwipeable } from 'react-swipeable';
 import Main from './Main';
 
 function SwipeDetector() {
-    const [activeCard, setActiveCard] = useState(1);
+    const [backgroundColor, setBackgroundColor] = useState('transparent');
+    const [number, setNumber] =useState(0)
+    const [activeCard, setActiveCard] = useState(1); 
 
     // Function to handle the scroll event
     function isElementOnScreen(element) {
@@ -16,72 +18,278 @@ function SwipeDetector() {
         );
     }
 
-    const scrollToSection = (sectionId) => {
-        const section = document.getElementById(sectionId);
-        if (section) {
-            section.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    function isDivAtTopOfScreen(divId) {
+        const div = document.getElementById(divId);
+    
+        if (div) {
+            const rect = div.getBoundingClientRect();
+    
+            // Check if the top of the div is in the current viewport
+            return rect.top >= 0 && rect.bottom >= 0;
         }
-    };
-
+    
+        // If the div doesn't exist, return false
+        return false;
+    }
     const handlers = useSwipeable({
         onSwiped: (eventData) => {
             if (eventData.dir === 'Up') {
-                if (activeCard < 9) {
-                    setActiveCard(activeCard + 1);
-                }
-            } else if (eventData.dir === 'Down') {
-                if (activeCard > 1) {
-                    setActiveCard(activeCard - 1);
-                }
+                setNumber(number+1)
+                    const notLeftRail = document.querySelector('.content-div:not(#left-rail):hover');
+                
+                    if (eventData.event.target.closest("#left-rail")) {
+                        // If the user is inside the #left-rail div, scroll to the top of #about plus 20px
+                        const aboutDiv = document.getElementById('about');
+                        const aboutDivTop = aboutDiv.getBoundingClientRect().top + window.scrollY;
+                        aboutDiv.scrollIntoView({behavior:'smooth', block:'start'})
+                    }
+                
+                    // if (notLeftRail) {
+                    //     // Scroll to the bottom of the current content div
+                    //     const notLeftRailBottom = notLeftRail.getBoundingClientRect().bottom;
+                    //     window.scrollBy(0, notLeftRailBottom);
+                    // }
+
+                    const inAbout = eventData.event.target.closest("#about");
+            
+                    if (inAbout) {
+                        // If the user is inside the #about div, scroll to the top of #experience plus 20px
+                        const experienceDiv = document.getElementById('experience');
+                        const experienceDivTop = experienceDiv.getBoundingClientRect().top + window.scrollY;
+                        experienceDiv.scrollIntoView({behavior:'smooth', block:'start'})
+                    }
+
+                    const inExperience = eventData.event.target.closest("#experience");
+            
+                    if (inExperience) {
+                        // If the user is inside the #experience div, scroll to the bottom of the #resume-blank
+                        const resumeDiv = document.getElementById('resume-blank');
+                        const resumeDivBottom = resumeDiv.getBoundingClientRect().bottom;
+
+                        if(isElementOnScreen(resumeDiv)) {
+                            const skillServiceDiv = document.getElementById('skill-service');
+                            const skillServiceDivTop = skillServiceDiv.getBoundingClientRect().top + window.scrollY;
+                            skillServiceDiv.scrollIntoView({behavior:'smooth', block:'start'})
+                        }
+                        else{
+                            resumeDiv.scrollIntoView({ behavior: 'smooth', block: 'end' });
+                        }
+                    }
+
+                    const inSkillServiceDiv = eventData.event.target.closest("#skill-service");
+            
+                    if (inSkillServiceDiv) {
+                        // If the user is inside the #experience div, scroll to the bottom of the #skills-blank
+                        const blank = document.getElementById('skills-blank');
+                        const blankDivBottom = blank.getBoundingClientRect().bottom;
+
+                        if(isElementOnScreen(blank)) {
+                            const projectsDiv = document.getElementById('projects');
+                            const projectsDivTop = projectsDiv.getBoundingClientRect().top + window.scrollY;
+                            projectsDiv.scrollIntoView({ behavior: 'smooth', block: 'end' });
+                        } else {
+                            blank.scrollIntoView({ behavior: 'smooth', block: 'end' });
+                        }
+                    }
+
+                    const inProjectsDiv = eventData.event.target.closest("#projects");
+
+                    if (inProjectsDiv) {
+                        if (activeCard > 9) {
+                            const contact = document.getElementById('contact');
+                            setActiveCard(9);
+                            contact.scrollIntoView({behavior:'smooth', block:'start'})
+                        } else {
+                            let projectCard = document.getElementById(`project${activeCard}`);
+                            if (projectCard !== null) {
+                                // Scroll to the project card
+                                projectCard.scrollIntoView({ behavior: 'smooth', block: 'start' });
+                            } else {
+                                console.log(`Project card with ID project${activeCard} not found.`);
+                            }
+                            
+                            setActiveCard(activeCard + 1);
+                        }
+                        
+                    }
+
+                    const incontact = eventData.event.target.closest("#contact");
+            
+                    if (incontact) {
+                        const blankDiv = document.getElementById('contact-blank');
+                        const blankDivTop = blankDiv.getBoundingClientRect().top + window.scrollY;
+                        blankDiv.scrollIntoView({ behavior: 'smooth', block: 'start' });
+                    }    
             }
+            
+            else if (eventData.dir === 'Down') {
+                    setNumber(number-1)
+                    if (eventData.event.target.closest("#left-rail")) {
+                        // If the user is inside the #left-rail div, scroll to the top of #about plus 20px
+                        // const aboutDiv = document.getElementById('about');
+                        // const aboutDivTop = aboutDiv.getBoundingClientRect().top + window.scrollY;
+                        // window.scrollTo({ top: aboutDivTop + 20, behavior: 'smooth' });
+                    }
+
+                    const incontact = eventData.event.target.closest("#contact");
+
+                    if (incontact) {
+                        const isDivAtTop = isDivAtTopOfScreen('contact');
+                        const contact = document.getElementById('contact');
+                        const lastProject = document.getElementById(`project${activeCard}`);
+
+                        if (lastProject !== null) {
+                            if(isElementOnScreen(lastProject)) {
+                                lastProject.scrollIntoView({ behavior: 'smooth', block: 'start' });
+                                setActiveCard(activeCard-1)
+                            }
+                        }
+                        if (isDivAtTop) {
+                            if (lastProject !== null) {
+                                // Scroll to the top of the project
+                                lastProject.scrollIntoView({ behavior: 'smooth', block: 'start' });
+                    
+                                // Wait for the scroll to finish before checking if it's at the top
+                                setTimeout(() => {
+                                    const isProjectAtTop = isDivAtTopOfScreen(`project${activeCard}`);
+                                    if (isProjectAtTop) {
+                                        // Scroll to lastProject if it's at the top
+                                        const nextProject = document.getElementById(`project${activeCard + 1}`);
+                                        if (nextProject !== null) {
+                                            nextProject.scrollIntoView({ behavior: 'smooth', block: 'start' });
+                                        }
+                                    }
+                                }, 500); // Adjust the timeout duration as needed
+                            }
+                        } else {
+                            contact.scrollIntoView({ behavior: 'smooth', block: 'start' });
+                        }
+                    }
+
+                    const inProjectsDiv = eventData.event.target.closest("#projects");
+
+                    if (inProjectsDiv) {
+                        // Check if the 'projects' div is at the top of the screen
+                        const isProjectsAtTop = isDivAtTopOfScreen('projects');
+                    
+                        if (isProjectsAtTop) {
+                            const skillServiceDiv = document.getElementById('skill-service');
+                            if (skillServiceDiv) {
+                                // Scroll to the top of the 'skill-service' div
+                                skillServiceDiv.scrollIntoView({ behavior: 'smooth', block: 'start' });
+                            }
+                        } else {
+                            if (activeCard <= 1) {
+                                // If 'activeCard' is less than or equal to 1, scroll to 'projects' and set 'activeCard' to 1
+                                const projectsDiv = document.getElementById('projects');
+                                if (projectsDiv) {
+                                    const projectsDivTop = projectsDiv.getBoundingClientRect().top + window.scrollY;
+                                    window.scrollTo({ top: projectsDivTop, behavior: 'smooth' });
+                                }
+                                setActiveCard(1);
+                            } else {
+                                // Scroll to the previous project card
+                                const projectCard = document.getElementById(`project${activeCard - 1}`);
+                                if (projectCard) {
+                                    projectCard.scrollIntoView({ behavior: 'smooth', block: 'start' });
+                                } else {
+                                    console.log(`Project card with ID project${activeCard - 1} not found.`);
+                                }
+                                setActiveCard(activeCard - 1);
+                            }
+                        }
+                    }
+
+                    const inSkillServiceDiv = eventData.event.target.closest("#skill-service");
+            
+                    if (inSkillServiceDiv) {
+                        const isSkillServiceTop = isDivAtTopOfScreen('skill-service');
+                    
+                        if (isSkillServiceTop) {
+                            const experienceDiv = document.getElementById('experience');
+                            if (experienceDiv) {
+                                // Scroll to the top of the 'skill-service' div
+                                experienceDiv.scrollIntoView({ behavior: 'smooth', block: 'start' });
+                            }
+                        } else {
+                            const skillServiceDiv = document.getElementById('skill-service');
+                            if (skillServiceDiv) {
+                                const skillServiceDivTop = skillServiceDiv.getBoundingClientRect().top + window.scrollY;
+                                window.scrollTo({ top: skillServiceDivTop, behavior: 'smooth' });
+                            }
+                        } 
+                    }
+
+                    const inexperienceDiv = eventData.event.target.closest("#experience");
+            
+                    if (inexperienceDiv) {
+                        const isexperienceTop = isDivAtTopOfScreen('experience');
+                        const aboutDiv = document.getElementById('about');
+                    
+                        if (isexperienceTop) {
+                            if (aboutDiv) {
+                                aboutDiv.scrollIntoView({ behavior: 'smooth', block: 'start' });
+                            
+                            } else {
+                                console.error("Element not found. Make sure the ID is correct.");
+                            }
+                        } else {
+                            const experienceDiv = document.getElementById('experience');
+                            console.log('here')
+                            if (experienceDiv) {
+                                const experienceDivTop = experienceDiv.getBoundingClientRect().top + window.scrollY;
+                                window.scrollTo({ top: experienceDivTop, behavior: 'smooth' });
+                            }
+                        } 
+                        
+                        if(isDivAtTopOfScreen('about')) {
+                            const leftRailDiv = document.getElementById('left-rail');
+                            if (leftRailDiv) {
+                                leftRailDiv.scrollIntoView({ behavior: 'smooth', block: 'start' });
+                            
+                            } else {
+                                console.error("Element not found. Make sure the ID is correct.");
+                            }
+                        }
+                    }
+
+                    const inaboutDiv = eventData.event.target.closest("#about");
+            
+                    if (inaboutDiv) {
+                        const isaboutTop = isDivAtTopOfScreen('about');
+                    
+                        if (isaboutTop) {
+                            const leftRailDiv = document.getElementById('left-rail');
+                            if (leftRailDiv) {
+                                leftRailDiv.scrollIntoView({ behavior: 'smooth', block: 'start' });
+                            
+                            } else {
+                                console.error("Element not found. Make sure the ID is correct.");
+                            }
+                        } else {
+                            const aboutDiv = document.getElementById('about');
+                            if (aboutDiv) {
+                                const aboutDivTop = aboutDiv.getBoundingClientRect().top + window.scrollY;
+                                window.scrollTo({ top: aboutDivTop, behavior: 'smooth' });
+                            }
+                        } 
+                    }
+
+                }
         },
     });
 
-    useEffect(() => {
-        switch (activeCard) {
-            case 1:
-                scrollToSection('about');
-                break;
-            case 2:
-                scrollToSection('experience');
-                break;
-            case 3:
-                scrollToSection('resume-blank');
-                break;
-            case 4:
-                scrollToSection('skill-service');
-                break;
-            case 5:
-                scrollToSection('skills-blank');
-                break;
-            case 6:
-                scrollToSection('projects');
-                break;
-            case 7:
-                scrollToSection('project7');
-                break;
-            case 8:
-                scrollToSection('project8');
-                break;
-            case 9:
-                scrollToSection('contact');
-                break;
-            default:
-                break;
-        }
-    }, [activeCard]);
+  const swipeAreaStyle = {
+    width: '100%',
+    maxHeight:'100vh',
+    height:'100vh'
+  };
 
-    const swipeAreaStyle = {
-        width: '100%',
-        height: '100vh',
-        overflow: 'hidden',
-    };
-
-    return (
+  return (
         <div {...handlers} style={swipeAreaStyle}>
-            <Main />
+            <Main/>
         </div>
-    );
+  );
 }
 
 export default SwipeDetector;
