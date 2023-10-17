997e929f67b1fa6a118aa902cc382f39a16e2b4e
paulthechampion <enwerempaulo@gmail.com>
2023-10-16 20:55:28 -0400

    Still fixibg bug 2

--- src/components/SwipeDetector.js
+++ src/components/SwipeDetector.js
@@ -4,50 +4,41 @@ import { Swipe } from 'react-swipe-component';
 class SwipeDetector extends Component {
   constructor() {
     super();
-    this.onSwipeLeftListener = this.onSwipeLeftListener.bind(this);
-    this.onSwipeRightListener = this.onSwipeRightListener.bind(this);
-    this.onSwipeDownListener = this.onSwipeDownListener.bind(this);
-    this.onSwipeUpListener = this.onSwipeUpListener.bind(this);
-    this.onSwipeListener = this.onSwipeListener.bind(this);
+    this.state = {
+      backgroundColor: 'lightgray',
+    };
+  }
+
+  onSwipeUpListener = () => {
+    this.setState({ backgroundColor: 'red' });
+  }
+
+  onSwipeDownListener = () => {
+    this.setState({ backgroundColor: 'green' });
   }
 
   render() {
+    const { backgroundColor } = this.state;
+
+    const swipeAreaStyle = {
+      width: '100%',
+      height: '300px',
+      backgroundColor,
+    };
+
     return (
       <Swipe
         nodeName="div"
-        id="swipe"
         className="swipe"
         mouseSwipe={false}
-        onSwipedLeft={this.onSwipeLeftListener}
-        onSwipedRight={this.onSwipeRightListener}
-        onSwipedDown={this.onSwipeDownListener}
         onSwipedUp={this.onSwipeUpListener}
-        onSwipe={this.onSwipeListener}
+        onSwipedDown={this.onSwipeDownListener}
       >
-        Swipe
+        <div style={swipeAreaStyle}>
+          Swipe me up or down
+        </div>
       </Swipe>
     );
-  }
-
-  onSwipeLeftListener() {
-    console.log('Swiped left');
-  }
-
-  onSwipeRightListener() {
-    console.log('Swiped right');
-  }
-
-  onSwipeUpListener() {
-    document.getElementsByClassName('swipe')[0].style.backgroundColor = 'red';
-  }
-
-  onSwipeDownListener() {
-    document.getElementsByClassName('swipe')[0].style.backgroundColor = 'green';
-  }
-
-  onSwipeListener(e) {
-    if (e[1] === 0) console.log('Swipe x: ' + e[0]);
-    else if (e[0] === 0) console.log('Swipe y: ' + e[1]);
   }
 }
 
