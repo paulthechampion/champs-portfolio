11b38b1181affef734251b11a95a5ce5d7d8707d
paulthechampion <enwerempaulo@gmail.com>
2023-10-17 00:39:30 -0400

    sighjjjjjjjjjjjjjjjjjjjjj

--- src/components/SwipeDetector.js
+++ src/components/SwipeDetector.js
@@ -3,16 +3,7 @@ import { useSwipeable } from 'react-swipeable';
 
 function SwipeDetector() {
     const [activeCard, setActiveCard] = useState(1);
-    const [backgroundColor,setBackgroundColor] = useState('white')
-
-    const swipeAreaStyle = {
-        width: '100%',
-        height: '100vh',
-        backgroundColor,
-        zIndex: 1,
-        position: 'fixed',
-        top: 0,
-    };
+    const [backgroundColor, setBackgroundColor] = useState(1);
 
     const scrollToElement = (elementId) => {
         const element = document.getElementById(elementId);
@@ -26,57 +17,23 @@ function SwipeDetector() {
             if (eventData.dir === 'Up') {
                 setBackgroundColor('red')
                 // Handle swipe up
-                if (activeCard === 1) {
-                    scrollToElement('contact');
-                } else if (activeCard === 2) {
+                if (activeCard > 1) {
                     scrollToElement(`project${activeCard - 1}`);
                     setActiveCard(activeCard - 1);
-                } else if (activeCard === 3) {
-                    scrollToElement(`project${activeCard - 1}`);
-                    setActiveCard(activeCard - 1);
-                } else if (activeCard === 4) {
-                    scrollToElement('skill-service');
-                    setActiveCard(3);
-                } else if (activeCard === 5) {
-                    scrollToElement('experience');
-                    setActiveCard(4);
-                } else if (activeCard === 6) {
-                    scrollToElement('about');
-                    setActiveCard(5);
-                } else if (activeCard === 7) {
-                    scrollToElement('about');
-                    setActiveCard(5);
                 }
             } else if (eventData.dir === 'Down') {
                 setBackgroundColor('yellow')
                 // Handle swipe down
-                if (activeCard === 1) {
+                if (activeCard < 7) {
                     scrollToElement(`project${activeCard}`);
                     setActiveCard(activeCard + 1);
-                } else if (activeCard === 2) {
-                    scrollToElement(`project${activeCard}`);
-                    setActiveCard(activeCard + 1);
-                } else if (activeCard === 3) {
-                    scrollToElement('skill-service');
-                    setActiveCard(4);
-                } else if (activeCard === 4) {
-                    scrollToElement('experience');
-                    setActiveCard(5);
-                } else if (activeCard === 5) {
-                    scrollToElement('about');
-                    setActiveCard(6);
-                } else if (activeCard === 6) {
-                    scrollToElement('about');
-                    setActiveCard(6);
-                } else if (activeCard === 7) {
-                    scrollToElement('contact');
                 }
             }
         },
     });
 
     return (
-        <div {...handlers} id="swipe-div" style={swipeAreaStyle}>
+        <div {...handlers} id="swipe-div" style={{ width: '100%', height: '100vh', backgroundColor, zIndex: 1, position: 'fixed', top: 0 }}>
             {/* Your swipeable content */}
         </div>
     );
