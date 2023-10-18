53de8d25113b2eda52b82a01b47485e629ef1886
paulthechampion <enwerempaulo@gmail.com>
2023-10-17 22:36:15 -0400

    Stopping here tonight 2

--- src/components/SkillsService.js
+++ src/components/SkillsService.js
@@ -1,4 +1,4 @@
-import React , { useEffect, useRef, useState }from 'react'
+import React , { useEffect, useRef }from 'react'
 import { useMediaQuery } from 'react-responsive'
 
 export default function SkillsService() {
@@ -8,48 +8,52 @@ export default function SkillsService() {
 
       function LoadingDiv({ loadingPercentage }) {
         const loadingStyle = {
-          width: `${loadingPercentage * 10}%`,
+          width: `${0}%`, // Initially, set the width to 0%
         };
       
-        return (
-          <div className="loading-container appear">
-            <div className="loading-bar" style={loadingStyle}></div>
-          </div>
-        );
-      }
-      
-      function DataPointList({ dataPoints }) {
-        const loadingFlexRef = useRef(null);
-        const [isVisible, setIsVisible] = useState(false);
+        const loadingRef = useRef(null);
       
         useEffect(() => {
+          const rootMargin = isDesktopOrLaptop ? "0px 0px -250px 0px" : "0px"
           const observer = new IntersectionObserver(handleIntersection, {
             root: null,
-            rootMargin: "0px",
-            threshold: 0.1, // Adjust this threshold as needed
+            rootMargin,
+            threshold: 0, // Trigger when 10% of the element is visible
           });
       
-          if (loadingFlexRef.current) {
-            observer.observe(loadingFlexRef.current);
+          if (loadingRef.current) {
+            observer.observe(loadingRef.current);
           }
       
           return () => {
-            if (loadingFlexRef.current) {
-              observer.unobserve(loadingFlexRef.current);
+            if (loadingRef.current) {
+              observer.unobserve(loadingRef.current);
             }
           };
       
           function handleIntersection(entries) {
             entries.forEach((entry) => {
               if (entry.isIntersecting) {
-                setIsVisible(true);
+                entry.target.style.transition = "width 1.5s ease-in";
+                entry.target.style.width = `${loadingPercentage * 10}%`;
+              }
+              else {
+                entry.target.style.width = 0;
               }
             });
           }
-        }, []);
+        }, [loadingPercentage]);
       
         return (
-          <div className={`loading-flex ${isVisible ? "appear" : ""}`} ref={loadingFlexRef}>
+          <div className="loading-container appear">
+            <div className="loading-bar" style={loadingStyle} ref={loadingRef}></div>
+          </div>
+        );
+      }
+      
+      function DataPointList({ dataPoints }) {
+        return (
+          <div className="loading-flex">
             {dataPoints.map((dataPoint, index) => (
               <div key={index}>
                 {dataPoint.name} <span className="skill-per">{dataPoint.number * 10}%</span>
@@ -59,8 +63,6 @@ export default function SkillsService() {
           </div>
         );
       }
-      
-
       const dataPoints = [
         { name: "HTML 5", number: 10 },
         { name: "C#", number: 4 },
