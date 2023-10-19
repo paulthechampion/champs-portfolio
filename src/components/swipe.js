a969a008c210d537705e3c7c9259a6a2b12539eb
paulthechampion <enwerempaulo@gmail.com>
2023-10-19 15:28:30 -0400

    please work

--- src/components/swipe.js
+++ src/components/swipe.js
@@ -1,45 +1,29 @@
-const sections = ["left-rail", "about", "experience", "skill-service", "projects", "contact"];
-
 export function moveUp(elementId) {
-    const sectionToAppear = document.getElementById(elementId);
-    const cardOnTop = sections.indexOf(elementId)
-    const cardOnTopDiv = document.getElementById(sections[cardOnTop-1])
-  
-    if (sectionToAppear) {
-      // Initially set the section at the bottom of the page
-      sectionToAppear.style.position = 'fixed';
-      sectionToAppear.style.bottom = 'unset'; 
-      sectionToAppear.style.top = '100%'; // Start at the bottom
-      sectionToAppear.style.transition = 'top 0.5s';
-      
-      console.log('saming', cardOnTopDiv)
-  
-      // Use a timeout to trigger the animation
-      setTimeout(() => {
-        // Set the top property to 0 to move the element to the top
-        sectionToAppear.style.top = '0';
-      }, 100);
-    }
+  const sectionToAppear = document.getElementById(elementId);
+
+  if (sectionToAppear) {
+    // Add a class to hide the section at the top
+    sectionToAppear.classList.add('hidden-up');
+
+    // Use a timeout to trigger the animation
+    setTimeout(() => {
+      // Remove the class to move the element into view
+      sectionToAppear.classList.add('hidden-up-show');
+    }, 500);
   }
-  
-  
+}
+
 export function moveDown(elementId) {
   const sectionToAppear = document.getElementById(elementId);
-  
-    if (sectionToAppear) {
-      // Initially set the section at the top of the page
-      sectionToAppear.style.position = 'fixed';
-      sectionToAppear.style.top = 'unset'; 
-      sectionToAppear.style.bottom = '100%'
-      // Start at the top
-      sectionToAppear.style.transition = 'bottom 1s';
-      
-  console.log("here", sectionToAppear)
-      // Use a timeout to trigger the animation
-      setTimeout(() => {
-        // Set the top property to 0 to move the element to the top
-        sectionToAppear.style.bottom = '0';
-      }, 100);
-    }
-  
+
+  if (sectionToAppear) {
+    // Add a class to hide the section at the bottom
+    sectionToAppear.classList.add('hidden-down');
+
+    // Use a timeout to trigger the animation
+    setTimeout(() => {
+      // Remove the class to move the element into view
+      sectionToAppear.classList.add('hidden-down-show');
+    }, 500);
+  }
 }
