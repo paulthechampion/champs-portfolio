caea0cf61011a46f30c3a30d5924c32fcf5ed2ad
Champion <enwerempaulo@gmail.com>
2024-06-17 17:15:09 -0700

    removed some lines

--- src/components/RightRail.js
+++ src/components/RightRail.js
@@ -25,8 +25,7 @@ export default function RightRail() {
 
             <p className='appear'>
                 My foundational knowledge comes from learning HTML, CSS, JavaScript and C# in 
-                Cnadian Business College, later dropped out to be self-taught. Beyond coding, I excel in creating responsive designs, ensuring seamless user 
-                experiences. My portfolio includes <span className='gold'>eCommerce websites, Investment research tools web applications, 
+                Cnadian Business College, later dropped out to be self-taught. My portfolio includes <span className='gold'>eCommerce websites, Investment research tools web applications, 
                 Chat applications, Business landing pages, and more, </span>  showcasing a multifaceted skill set that combines 
                 technical expertise with creative problem-solving. 
             </p>
